7686e4ce6c6216e4765c83483e6032f968e9418b
Chocoie <87912667+Chocoie@users.noreply.github.com>
2026-01-29 21:16:03 -0500

    styled login page -trevi

--- frontend/JS/code.js
+++ frontend/JS/code.js
@@ -56,3 +56,40 @@ function doLogin()
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
 }
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "login.html";
+	}
+}
